ee8cd7eb5883864400ae963d350051e274c3b988
tadd581 <nguyendat58211@gmail.com>
2023-01-01 20:24:49 +0700

    feat: setup follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :follows
   get "/search", to: "posts#search"
   resources :posts
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_073547) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_113852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
