ede51b95e6bc2a6f6abecf4e59238e1f722392f1
tadd581 <nguyendat58211@gmail.com>
2023-01-20 00:22:58 +0700

    chore: ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   def send_friend_request
     @user = User.find(params[:id])
     @user.followers << current_user
-    puts "send friends request #{params[:id]}"
     redirect_back(fallback_location: root_path)
+  end
+
+  def remove_friend_request
+    @request = Follow.find(params[:id])
   end
 end
